78fcb736baf165e02334ec496d5befdbbdef488f
Gib <celxd250606@gmail.com>
2023-03-14 19:57:04 +0700

    Game manager for pause, resume, and game over

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -47,81 +47,84 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        activeCam = CameraManager.active;
-        noise = activeCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
-        Vector3 moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-        Vector3 moveVelocity = moveDirection * speed;
-
-        rb.AddForce(Physics.gravity * gravityMultiplier, ForceMode.Acceleration);
-
-        if (Input.GetKey(KeyCode.LeftShift) && canRun == true)
+        if(GameManager.Instance.isOver == false)
         {
-            rb.velocity = transform.TransformDirection(moveVelocity * 1.5f);
-            currentStamina -= staminaDepletionRate * Time.deltaTime;
-            regenStamina = true;
+            activeCam = CameraManager.active;
+            noise = activeCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+            Vector3 moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+            Vector3 moveVelocity = moveDirection * speed;
 
-            if (CameraManager.IsActiveCamera(cam_sprint) != true)
-                CameraManager.SwitchCamera(cam_sprint);
-        }
-        else
-        {
-            
-            
-            if (regenStamina)
-                currentStamina += staminaRegenRate * Time.deltaTime;
+            rb.AddForce(Physics.gravity * gravityMultiplier, ForceMode.Acceleration);
 
-            if(Input.GetMouseButton(1))
+            if (Input.GetKey(KeyCode.LeftShift) && canRun == true)
             {
-                rb.velocity = transform.TransformDirection(moveVelocity / 1.5f);
-                if (CameraManager.IsActiveCamera(cam_zoom) != true)
-                    CameraManager.SwitchCamera(cam_zoom);
+                rb.velocity = transform.TransformDirection(moveVelocity * 1.5f);
+                currentStamina -= staminaDepletionRate * Time.deltaTime;
+                regenStamina = true;
+
+                if (CameraManager.IsActiveCamera(cam_sprint) != true)
+                    CameraManager.SwitchCamera(cam_sprint);
             }
-            
             else
             {
-                rb.velocity = transform.TransformDirection(moveVelocity);
-                if (CameraManager.IsActiveCamera(cam) != true)
-                    CameraManager.SwitchCamera(cam);
+
+
+                if (regenStamina)
+                    currentStamina += staminaRegenRate * Time.deltaTime;
+
+                if (Input.GetMouseButton(1))
+                {
+                    rb.velocity = transform.TransformDirection(moveVelocity / 1.5f);
+                    if (CameraManager.IsActiveCamera(cam_zoom) != true)
+                        CameraManager.SwitchCamera(cam_zoom);
+                }
+
+                else
+                {
+                    rb.velocity = transform.TransformDirection(moveVelocity);
+                    if (CameraManager.IsActiveCamera(cam) != true)
+                        CameraManager.SwitchCamera(cam);
+                }
             }
-        }
-        
-        if (currentStamina >= maxStamina)
-        {
-            regenStamina = false;
-            canRun = true;
-        }
-        
-        else if (currentStamina <= 0)
-        {
-            currentStamina = 0;
-            regenStamina = true;
-            canRun = false;
-        }
 
-        currentStamina = Mathf.Clamp(currentStamina, 0f, maxStamina);
-        
-        turn.x += Input.GetAxis("Mouse X") * sensitivity;
-        turn.y += Input.GetAxis("Mouse Y") * sensitivity;
-        turn.y = Mathf.Clamp(turn.y, -90, 90);
-        transform.eulerAngles = new Vector3(0, turn.x, 0);
-        activeCam.transform.localEulerAngles = new Vector3(-turn.y, 0, 0);
-        
-        float velocity = Mathf.Clamp01(Mathf.Abs(rb.velocity.x) + Mathf.Abs(rb.velocity.z));
-        if (velocity > 0)
-        {
-            noise.m_AmplitudeGain = velocity * headBobAmount * speed;
-            noise.m_FrequencyGain = velocity * headBobSpeed * speed;
-
-            if (Input.GetKey(KeyCode.LeftShift))
+            if (currentStamina >= maxStamina)
             {
-                noise.m_AmplitudeGain *= 1.5f;
-                noise.m_FrequencyGain *= 1.5f;
+                regenStamina = false;
+                canRun = true;
             }
-        }
-        else
-        {
-            noise.m_AmplitudeGain = headBobAmount;
-            noise.m_FrequencyGain = headBobSpeed;
+
+            else if (currentStamina <= 0)
+            {
+                currentStamina = 0;
+                regenStamina = true;
+                canRun = false;
+            }
+
+            currentStamina = Mathf.Clamp(currentStamina, 0f, maxStamina);
+
+            turn.x += Input.GetAxis("Mouse X") * sensitivity;
+            turn.y += Input.GetAxis("Mouse Y") * sensitivity;
+            turn.y = Mathf.Clamp(turn.y, -90, 90);
+            transform.eulerAngles = new Vector3(0, turn.x, 0);
+            activeCam.transform.localEulerAngles = new Vector3(-turn.y, 0, 0);
+
+            float velocity = Mathf.Clamp01(Mathf.Abs(rb.velocity.x) + Mathf.Abs(rb.velocity.z));
+            if (velocity > 0)
+            {
+                noise.m_AmplitudeGain = velocity * headBobAmount * speed;
+                noise.m_FrequencyGain = velocity * headBobSpeed * speed;
+
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    noise.m_AmplitudeGain *= 1.5f;
+                    noise.m_FrequencyGain *= 1.5f;
+                }
+            }
+            else
+            {
+                noise.m_AmplitudeGain = headBobAmount;
+                noise.m_FrequencyGain = headBobSpeed;
+            }
         }
     }
 
